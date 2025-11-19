cf9f1baeed2aa9968679bdf59fe0cb2fd7617184
KatITJ <katherynne.plessmann@itj.com>
2025-11-19 10:16:04 -0800

    add comment

--- tests/test_flaky.py
+++ tests/test_flaky.py
@@ -1,6 +1,7 @@
 import random, time
 
 
+# This test is intentionally flaky; it will pass approximately 50% of the time.
 def test_flaky():
     rnd = int(time.time() * 1_000_000) % 2
     assert rnd == 1
